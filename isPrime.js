1d7cd9aefe78668697f4da21f62836d9b5dabe95
nfatah <nfatah41@gmail.com>
2016-04-02 16:02:52 +0300

    refactor

--- isPrime.js
+++ isPrime.js
@@ -3,8 +3,7 @@ function isPrime(num) {
   for(var i = 1; i < 10; i++){
     if(num > 9){return num % i !== 0;}
     if(num % i === 0 && i !== 1){
-      if(i !== num){return false;}
-      else if( i === num){return true;}
+      return i === num;
     }
    }
   return num !== 1;
